11597d33013a17e74b88a46fb72cd26b6b866aff
nitish <nitishk12c@gmail.com>
2020-12-01 14:59:44 +0530

    src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import './App.css'
+import Login from './Login'
+import  { getTokenFromResponse } from './spotify'
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player'
+import { useDataLayerValue }  from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  
+  // const [token, setToken] = useState(null);
+
+  const [{ user ,token}, dispatch] = useDataLayerValue();
+ 
+  // run code based on a given conditions
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token){
+      // setToken(_token); 
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type:"SET_TOKEN",
+        token: _token,
+      })
+
+      spotify.getPlaylist("37i9dQZEVXcGLoJrWSLPSS").then((response) =>
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response,
+      })
+    );
+
+    spotify.getMyTopArtists().then((response) =>
+    dispatch({
+      type: "SET_TOP_ARTISTS",
+      top_artists: response,
+    })
+  );
+
+  dispatch({
+    type: "SET_SPOTIFY",
+    spotify: spotify,
+  });
+      spotify.getMe().then(user=>{
+       
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        })
+      });
+
+    
+
+      spotify.getUserPlaylists().then((playlists)=>{
+        dispatch({
+          type:"SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+  
+    }
+    
+
+  }, [token, dispatch]); 
+
+  
+  console.log("THis is token", token);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
     </div>
   );
-}
+
+  // return (
+  //   <div className="app">
+  //     {
+  //       token? (
+  //         <Player spotify={spotify} />  
+  //       ): (
+  //         <Login />
+
+  //       )
+  //     }
+  //   </div>
+  // );
+
+
+
+} 
 
 export default App;
